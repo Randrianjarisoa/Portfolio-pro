27586ea45c4f85c2266143f88a5ff5e6b5114964
randrianjarisoa <randrianjarisoa@gmail.com>
2025-06-19 22:26:12 +0300

    fix(resume): resolve issue & make responsive both aboutMe

--- src/portfolioContainer/resume/Resume.js
+++ src/portfolioContainer/resume/Resume.js
@@ -18,22 +18,21 @@ const Resume = (props) => {
     return (
       <div className="resume-heading">
         <div className="resume-main-heading">
-          <div className="heading-bullet">
-            <span>{props.heading ? props.heading : ""}</span>
-            {props.fromDate && props.toDate ? (
-              <div className="heading-date">
-                {props.fromDate + "-" + props.toDate}
-              </div>
-            ) : (
-              <div></div>
-            )}
-          </div>
-          <div className="resume-sub-heading">
-            <span>{props.subHeading ? props.subHeading : ""}</span>
-          </div>
-          <div className="resume-heading-description">
-            <span>{props.description ? props.description : ""}</span>
-          </div>
+          <div className="heading-bullet"></div>
+          <span>{props.heading ? props.heading : ""}</span>
+          {props.fromDate && props.toDate ? (
+            <div className="heading-date">
+              {props.fromDate + "-" + props.toDate}
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+        <div className="resume-sub-heading">
+          <span>{props.subHeading ? props.subHeading : ""}</span>
+        </div>
+        <div className="resume-heading-description">
+          <span>{props.description ? props.description : ""}</span>
         </div>
       </div>
     );
@@ -150,8 +149,8 @@ const Resume = (props) => {
           <span>{skill.skill}</span>
           <div className="skill-percentage">
             <div
+              className="active-percentage-bar"
               style={{ width: skill.ratingPercentage + "%" }}
-              className="active-percentage"
             ></div>
           </div>
         </div>
